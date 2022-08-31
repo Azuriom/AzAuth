3c4be4352850321ea7249feacbe8475e2b76a8b2
Arnaud Lier <arnaud@lier.link>
2022-08-31 12:44:41 +0200

    Trim trailing slash in base url (#8)

--- src/main/java/com/azuriom/azauth/AuthClient.java
+++ src/main/java/com/azuriom/azauth/AuthClient.java
@@ -52,7 +52,9 @@ public class AuthClient {
      * @param url the website url
      */
     public AuthClient(@NotNull String url) {
-        this.url = Objects.requireNonNull(url, "url");
+        Objects.requireNonNull(url, "url");
+
+        this.url = url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
 
         if (!url.startsWith("https://")) {
             LOGGER.warning("HTTP links are not secure, use HTTPS instead.");
